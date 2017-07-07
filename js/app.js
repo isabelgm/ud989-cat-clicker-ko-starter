04c82b8e6a85bde0a2dd1e2cbb0a7ad2780e0e68
isa <isabel@juntostudio.com>
2017-07-07 13:07:43 -0600

    Add levels to cat clicker

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var viewModel = function() {
+  //model goes in here
+  this.name = ko.observable('Bob');
+  this.clickCount = ko.observable(0);
+  this.imgSrc = ko.observable('img/cat1.jpg');
+  this.imgAttribution = ko.observable('https://flickr.com/photos');
+  this.level = ko.computed(function(){
+    if (this.clickCount()< 10) return "Newborn";
+    if (this.clickCount()>= 10 && this.clickCount() < 50) return "Infant";
+    if (this.clickCount()>= 50 && this.clickCount() < 100) return "Child";
+    if (this.clickCount()>= 100) return "Teen";
+  }, this);
+
+  this.incrementCounter = function() {
+    this.clickCount(this.clickCount() + 1);
+  };
+}
+
+ko.applyBindings(new viewModel());
